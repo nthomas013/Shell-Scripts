eee725aacbd71ea0e1a891367d8108332e072d1f
nthomas013 <nthomas095@yahoo.com>
2025-08-19 12:29:13 +0530

    Added my first simple shell script

---
+++ welcomescript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Enter name as input
+echo "Enter your names:"
+read var1 var2
+#prints out of variables var1 and var2 with the welcome message
+echo "Hello $var1 and $var2, Welcome to Synnefo Solutions"
